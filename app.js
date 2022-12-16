bee3cc19aeff1b10ba0b42ec66278134468bbeaa
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-12-16 17:19:01 -0500

    no changes on this object

--- app.js
+++ app.js
@@ -205,7 +205,24 @@ let noAlguien = ente.call( alguien,"Parchita");
 // console.log( noAlguien );
 
 
+var manejador = {
+    id: '123',
+    init: function() {
 
+        document.addEventListener( "click", 
+        event => this.clickEnPagina( event.type ) );
+
+        document.addEventListener( "click", (function(event) {
+            this.clickEnPagina( event.type );
+        }).bind( this ), false);
+    },
+
+    clickEnPagina:function( type ){
+        console.log( "Manejando" + type + " para el id: " + this.id );
+    }
+};
+
+manejador.init();
 
 
 
