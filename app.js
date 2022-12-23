aa0e4fcca1fab5e8085e27624269da4912abebe5
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-12-23 16:17:51 -0500

    object destructuring nested

--- app.js
+++ app.js
@@ -430,11 +430,44 @@ let {nombre, email, facebook,google, premium: dePago, twitter: cuentaTwi = "codi
 console.log(nombre, email, facebook, google, dePago, cuentaTwi );
 
 
+/* Desestructuracion de objetos anidados */
 
+let autoGuardado = {
 
+    archivo: "app.js",
+    cursor: {
+        linea: 7,
+        columna: 16
+    },
+    ultimoArchivo: {
+        archivo: "index.html",
+        cursor: {
+            linea: 8,
+            columna: 20
+        }
+    },
+    otroNodo: {
+        subNodo:{
+            cursor: {
+                linea:11,
+                columna:11
+            }
+        }
+    }
 
+};
 
+let { cursor: cursorActivo } = autoGuardado;
+console.log( cursorActivo );
 
+let { ultimoArchivo:{ cursor: ultimoArchivo }  } = autoGuardado;
+console.log( ultimoArchivo );
+
+let { otroNodo:{ subNodo: { cursor:otroSuperNodo } } } = autoGuardado;
+console.log( otroSuperNodo );
+
+let anotherSuperNodo = autoGuardado.otroNodo.subNodo.cursor;
+console.log(anotherSuperNodo  );
 
 
 
